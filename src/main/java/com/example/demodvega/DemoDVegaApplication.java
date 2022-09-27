0f69999e1cedf98e191aad24758bdc4591635117
Ryan Wakabayashi <ryan.wakabayashi@gmail.com>
2022-09-27 12:56:44 -0500

    Initial commit for spring boot reddit clone

---
+++ src/main/java/com/example/demodvega/DemoDVegaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodvega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDVegaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDVegaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demodvega/DemoDVegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodvega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDVegaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
