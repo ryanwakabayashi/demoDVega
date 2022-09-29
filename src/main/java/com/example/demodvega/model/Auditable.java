e3d2e6e4c47fb142b5ec27db15a25717b9a9d143
Ryan Wakabayashi <ryan.wakabayashi@gmail.com>
2022-09-29 07:46:24 -0500

    auditable added

---
+++ src/main/java/com/example/demodvega/model/Auditable.java
@@ -0,0 +1,30 @@
+package com.example.demodvega.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public abstract class Auditable {
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private String createdDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private String lastModifiedDate;
+
+}
